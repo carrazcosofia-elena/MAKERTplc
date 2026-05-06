de5b786b85feef8dcace3fe15491ce20cca7ad5f
carrazcosofia-elena <sofiacarrazcop@gmail.com>
2026-05-05 19:24:29 -0700

    actualizando

--- store/forms.py
+++ store/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from .models import User
+from .models import Product
 
 class RegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -9,3 +10,11 @@ class RegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'is_seller', 'password1', 'password2')
+        
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = ['name', 'description', 'price', 'stock', 'categories']
+        widgets = {
+            'categories': forms.CheckboxSelectMultiple()
+        }
