fd6287cfddf419dfcae3f7eb6b3d8723f0bb2099
carrazcosofia-elena <sofiacarrazcop@gmail.com>
2026-05-08 16:05:55 -0700

    m

--- store/models.py
+++ store/models.py
@@ -75,6 +75,9 @@ class Cart(models.Model):
     def __str__(self):
         return f"Cart {self.id} - {self.user}"
 
+    @property
+    def total(self):
+        return sum(item.subtotal for item in self.cartitem_set.all())
 
 # =========================
 # 🧾 CartItem (tabla intermedia)
@@ -92,3 +95,8 @@ class CartItem(models.Model):
 
     def __str__(self):
         return f"{self.product} x {self.quantity}"
+    @property
+    def subtotal(self):
+          return self.product.price * self.quantity
+    def __str__(self):
+       return f"{self.product} x {self.quantity}"
